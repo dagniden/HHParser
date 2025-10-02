91f9fb0186faf8392077bcea501191fefd1af736
Daniil Gnidenko <dagnidenko@gmail.com>
2025-10-02 21:12:10 +0300

    Исправлена фикстура и тесты для моделей вакансий

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,9 +2,14 @@ import pytest
 
 from src.models import Vacancy
 
+
 @pytest.fixture
 def vacancy() -> Vacancy:
-    return Vacancy(200,
-                   300,
-                   "Тестировщик",
-                   "Тестировать приложения лопатой")
+    return Vacancy('124937232',
+                   'https://api.hh.ru/vacancies/124937232?host=hh.ru',
+                   "Менеджер",
+                   'Ставить задачи, сопровождать и контролировать их выполнение . Удаленная работа.',
+                   'Е-Клиник',
+                   'Москва',
+                   125000,
+                   150000)
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,17 +1,23 @@
 from src.models import Vacancy, VacancyList
 
 
-def test_vacancy_valid() -> None:
-    vacancy1 = Vacancy(200, 300, "Тестировщик", "Тестировать приложения лопатой")
-    assert vacancy1.salary_from == 200
-    assert vacancy1.salary_to == 300
-    assert vacancy1.title == "Тестировщик"
-    assert vacancy1.description == "Тестировать приложения лопатой"
+def test_vacancy_valid(vacancy) -> None:
+    assert vacancy.vacancy_id == '124937232'
+    assert vacancy.vacancy_url == 'https://api.hh.ru/vacancies/124937232?host=hh.ru'
+    assert vacancy.title == "Менеджер"
+    assert vacancy.description == 'Ставить задачи, сопровождать и контролировать их выполнение . Удаленная работа.'
+    assert vacancy.company_name == 'Е-Клиник'
+    assert vacancy.area_name == 'Москва'
+    assert vacancy.salary_from == 125000
+    assert vacancy.salary_to == 150000
 
 
 def test_vacancy_str(vacancy: Vacancy) -> None:
-    assert str(vacancy) == "{'salary_from': 200, 'salary_to': 300, 'title': 'Тестировщик', 'description': 'Тестировать приложения лопатой'}"
-    assert repr(vacancy) == "{'salary_from': 200, 'salary_to': 300, 'title': 'Тестировщик', 'description': 'Тестировать приложения лопатой'}"
+    assert str(
+        vacancy) == "{'vacancy_id': '124937232', 'vacancy_url': 'https://api.hh.ru/vacancies/124937232?host=hh.ru', 'title': 'Менеджер', 'description': 'Ставить задачи, сопровождать и контролировать их выполнение . Удаленная работа.', 'company_name': 'Е-Клиник', 'area_name': 'Москва', 'salary_from': 125000, 'salary_to': 150000}"
+
+    assert repr(
+        vacancy) == "{'vacancy_id': '124937232', 'vacancy_url': 'https://api.hh.ru/vacancies/124937232?host=hh.ru', 'title': 'Менеджер', 'description': 'Ставить задачи, сопровождать и контролировать их выполнение . Удаленная работа.', 'company_name': 'Е-Клиник', 'area_name': 'Москва', 'salary_from': 125000, 'salary_to': 150000}"
 
 
 def test_vacancy_list(vacancy: Vacancy) -> None:
